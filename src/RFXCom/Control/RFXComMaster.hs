d0976117328d09997419d5085ce867c8f47a0fe1
Tomas Stenlund <tomas@laptop.stenlund.cc>
2016-08-19 05:51:41 -0500

    Continued working on the Master Monad

--- src/RFXCom/Control/RFXComMaster.hs
+++ src/RFXCom/Control/RFXComMaster.hs
@@ -9,7 +9,7 @@
 module RFXCom.Control.RFXComMaster (
   Handle(..),
   Config(..),
-  RFXCom.Control.RFXComMaster.Message(..),
+  Message(..),
   defaultConfig,
   withHandle,
   ) where
@@ -19,49 +19,34 @@ module RFXCom.Control.RFXComMaster (
 --
 import qualified System.IO                        as SIO
 
-import           Control.Concurrent               (ThreadId (..), killThread)
-import           Control.Concurrent.Chan          (Chan, newChan, readChan,
-                                                   writeChan)
-import           Control.Concurrent.MVar          (MVar, newEmptyMVar, newMVar,
-                                                   putMVar, takeMVar)
+import           Control.Concurrent               (ThreadId, killThread)
+import           Control.Concurrent.MVar          (MVar, newEmptyMVar, putMVar,
+                                                   takeMVar)
 
-import           Control.Exception
 import           Control.Monad
-import           Control.Monad.Managed            (Managed, managed, runManaged)
+import           Control.Monad.IO.Class           (MonadIO (..))
 import           Control.Monad.Reader             (MonadReader (..),
                                                    ReaderT (..), ask,
                                                    runReaderT)
 import           Control.Monad.State              (MonadState (..), StateT (..),
                                                    evalStateT, get, put)
-
-import           Pipes
-
-import qualified Pipes.Binary                     as PB (DecodingError (..),
-                                                         decodeGet)
-import qualified Pipes.ByteString                 as PBS (fromLazy, hGetSome,
-                                                          toHandle)
-import qualified Pipes.Parse                      as PP
-import qualified Pipes.Prelude                    as P (mapM, mapM_, repeatM)
-import           Pipes.Safe
-
-import           Data.ByteString                  (ByteString)
-import           Data.Word                        (Word8)
+import           Control.Monad.Trans.Class        (MonadTrans (..))
 
 --
 -- Internal import section
 --
 import qualified RFXCom.Control.RFXComWriter      as RFXComW (Handle (..),
                                                               Message (..))
-import           RFXCom.Message.Base
-import           RFXCom.Message.Decoder           (msgParser)
+
+import qualified          RFXCom.Message.Base              as MB
 import qualified RFXCom.Message.InterfaceControl  as IC (Body (..),
                                                          Command (..))
 import qualified RFXCom.Message.InterfaceResponse as IR (Body (..))
-import           RFXCom.System.Concurrent         (executeAfterDelay, forkChild,
-                                                   waitForChildren)
+
+import           RFXCom.System.Concurrent         (executeAfterDelay, forkChild)
 import           RFXCom.System.Exception          (ResourceException (..))
 import qualified RFXCom.System.Log                as Log (Handle (..), LoggerT,
-                                                          MonadLogger(..),
+                                                          MonadLogger (..),
                                                           runLoggerT, _debug,
                                                           _error, _info,
                                                           _warning)
@@ -79,21 +64,21 @@ instance Show (MVar a) where
   show m = "MVar"
 
 -- |The messages to the master thread
-data Message = Message RFXCom.Message.Base.Message -- ^Any incoming message from the RFXCom device
+data Message = Message MB.Message -- ^Any incoming message from the RFXCom device
              | Timeout          -- ^The timeout message, acts as a heartbeat
              | Stop (MVar ())   -- ^Shuts down the mater threads
              deriving (Show)
 
 -- |The service handle to the communcation processes.
 data Handle = Handle {
-  send::RFXCom.Control.RFXComMaster.Message->IO () -- ^The sender function of commands to the thread
+  send::Message->IO () -- ^The sender function of commands to the thread
   }
 
 -- |The internal service handle to the communcation processes.
 data Environment = Environment {
   loggerH   :: Log.Handle        -- ^The handle to the logger service
   , writerH :: RFXComW.Handle  -- ^The handle to the RFXCom Device Writer process
-  , mvar    :: MVar RFXCom.Control.RFXComMaster.Message}   -- ^The communcation mvar
+  , mvar    :: MVar Message}   -- ^The communcation mvar
 
 
 -- |The state machines states
@@ -122,8 +107,8 @@ withHandle::Config          -- ^The configuration of the master threads
 withHandle config loggerH writerH io = do
   env <- Environment loggerH writerH <$> newEmptyMVar
   tid <- forkChild $ masterThread env
-  x <- io $ Handle { send = sendToMaster env}
-  s <- stopMasterThread env
+  x <- io $ Handle { send = sendToMaster (mvar env)}
+  s <- stopMasterThread (mvar env)
   return x
 
 --
@@ -147,36 +132,35 @@ runRFXComMaster::(Monad m) => RFXComMaster m a -- ^The RFXCom Master monad
                ->m a              -- ^The result
 runRFXComMaster (RFXComMaster m) env state = Log.runLoggerT (evalStateT (runReaderT m env) state) (loggerH env)
 
---
--- The serial port writer functions
---
 
 -- |Sends a message to the master thread.
-sendToMaster::Environment                         -- ^The internal handle to the master thread
-            ->RFXCom.Control.RFXComMaster.Message -- ^Tĥe message to send
+sendToMaster::MVar Message -- ^The internal handle to the master thread
+            ->Message      -- ^Tĥe message to send
             ->IO ()
-sendToMaster env msg = do
-  putMVar (mvar env) msg
+sendToMaster mvar msg = do
+  putMVar mvar msg
 
 -- |Stops the master thread by sending a Stop message. This function do not return until the thread
 -- has acknowledged the Stop message.
-stopMasterThread::Environment -- ^The internal handle to the master thread
+stopMasterThread::MVar Message -- ^The internal handle to the master thread
                 -> IO ()
-stopMasterThread ih = do
+stopMasterThread mvar = do
   s<-newEmptyMVar
-  sendToMaster ih $ Stop s
+  sendToMaster mvar $ Stop s
   takeMVar s
+
 
 -- |Schedules a timeout to be sent to the master thread.
 scheduleTimeout::(Monad m, MonadIO m)=>Int -- ^The timeout in microseconds
-               -> RFXComMaster m () 
+               -> RFXComMaster m ()
 scheduleTimeout t = do
   env <- ask
   state <- get
-  tid <- liftIO $ executeAfterDelay t $ sendToMaster env Timeout
+  tid <- liftIO $ executeAfterDelay t $ sendToMaster (mvar env) Timeout
   put $ state {timer = Just tid}
   Log.info $ "RFXCom.Control.RFXComMaster.scheduleTimeout: Scheduling a timeout for " ++ show t ++ " " ++ show tid
   return ()
+
 
 -- |Cancels the timeout
 cancelTimeout::(Monad m, MonadIO m)=>RFXComMaster m ()
@@ -195,15 +179,14 @@ executeStateMachine::(Monad m, MonadIO m)=>MachineState    -- ^The current state
 --
 -- State: Start Signal: anything
 --
--- Flush the serial port and send the Get Status command to the RFXCom device
+-- Send the reset command
 --
 executeStateMachine Start _ = do
   env <- ask
   state <- get
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: Started"
   liftIO $ putStrLn "Resetting the RFXComDevice"
-  liftIO $ RFXComW.send (writerH env) $ RFXComW.Flush
-  liftIO $ RFXComW.send (writerH env) $ RFXComW.Message $ InterfaceControl IC.Body {IC._cmnd = IC.Reset}
+  liftIO $ RFXComW.send (writerH env) $ RFXComW.Message $ MB.InterfaceControl IC.Body {IC._cmnd = IC.Reset}
   scheduleTimeout 1000000
   put state {machineState = ResetTheDevice}
   return ()
@@ -219,7 +202,7 @@ executeStateMachine ResetTheDevice Timeout = do
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: ResetTheDevice got Timeout (" ++ show (timer state) ++ ")"
   liftIO $ RFXComW.send (writerH env) RFXComW.Flush
   liftIO $ putStrLn "RFXCom Device is reset"
-  liftIO $ RFXComW.send (writerH env) $ RFXComW.Message $ InterfaceControl IC.Body {IC._cmnd = IC.GetStatus}
+  liftIO $ RFXComW.send (writerH env) $ RFXComW.Message $ MB.InterfaceControl IC.Body {IC._cmnd = IC.GetStatus}
   scheduleTimeout 1000000
   put state {machineState = WaitForStatusFromTheDevice}
   return ()
@@ -230,14 +213,14 @@ executeStateMachine ResetTheDevice Timeout = do
 -- We got what we wanted, so cancel the current timer and send a start receiver command to the RFXCom device
 -- and reschedule a new timer for the response.
 --
-executeStateMachine WaitForStatusFromTheDevice msg@(RFXCom.Control.RFXComMaster.Message (InterfaceResponse IR.Body {IR._cmnd=2})) = do
+executeStateMachine WaitForStatusFromTheDevice msg@(Message (MB.InterfaceResponse IR.Body {IR._cmnd=2})) = do
   env <- ask
-  state <- get  
+  state <- get
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: WaitForStatusFromTheDevice got Message (" ++ show (timer state) ++ ")"
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: " ++ show msg
   cancelTimeout
   liftIO $ putStrLn "RFXCom Device status is ok"
-  liftIO $ RFXComW.send (writerH env) $ RFXComW.Message $ InterfaceControl IC.Body {IC._cmnd = IC.Start}
+  liftIO $ RFXComW.send (writerH env) $ RFXComW.Message $ MB.InterfaceControl IC.Body {IC._cmnd = IC.Start}
   scheduleTimeout 1000000
   put state {machineState = WaitForStartFromTheDevice}
   return ()
@@ -247,8 +230,8 @@ executeStateMachine WaitForStatusFromTheDevice msg@(RFXCom.Control.RFXComMaster.
 --
 -- Ignore any other message that is not a response to the get status message. Just continue waiting.
 --
-executeStateMachine WaitForStatusFromTheDevice msg@(RFXCom.Control.RFXComMaster.Message _ ) = do
-  state <- get  
+executeStateMachine WaitForStatusFromTheDevice msg@(Message _ ) = do
+  state <- get
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: WaitForStatusFromTheDevice ignoring wrong message (" ++ show (timer state) ++ ")"
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: " ++ show msg
   return ()
@@ -261,23 +244,20 @@ executeStateMachine WaitForStatusFromTheDevice msg@(RFXCom.Control.RFXComMaster.
 --
 executeStateMachine WaitForStatusFromTheDevice Timeout = do
   env <- ask
-  state <- get  
+  state <- get
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: WaitForStatusFromTheDevice got Timeout (" ++ show (timer state) ++ ")"
-  liftIO $ putStrLn "RFXCom Device timeout, resetting"
-  liftIO $ RFXComW.send (writerH env) $ RFXComW.Message $ InterfaceControl IC.Body {IC._cmnd = IC.Reset}
-  scheduleTimeout 1000000
-  put state {machineState = ResetTheDevice}
+  put state {machineState = Start}
   return ()
 
 --
--- State: WaitForStartFromDevice Signal: Message from RFXComDevice InterfaceControlResponse
+-- State: WaitForStartFromDevice Signal: Message from RFXCom device InterfaceControlResponse
 --
 -- We got a start acknowledge from the RFXCom device, so we are now good to go. Start waiting for messages from
 -- the RFXCom device or the MQTT broker.
 --
-executeStateMachine WaitForStartFromTheDevice  msg@(RFXCom.Control.RFXComMaster.Message (InterfaceResponse IR.Body {IR._cmnd=7})) = do
+executeStateMachine WaitForStartFromTheDevice  msg@(Message (MB.InterfaceResponse IR.Body {IR._cmnd=7})) = do
   env <- ask
-  state <- get  
+  state <- get
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: WaitForStartFomTheDevice got Message (" ++ show (timer state) ++ ")"
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: " ++ show msg
   liftIO $ putStrLn "RFXCom Device started"
@@ -285,31 +265,38 @@ executeStateMachine WaitForStartFromTheDevice  msg@(RFXCom.Control.RFXComMaster.
   put state {machineState = WaitForMessage, timer = Nothing}
   return ()
 
-executeStateMachine WaitForStartFromTheDevice  msg@(RFXCom.Control.RFXComMaster.Message _) = do
+--
+-- State: WaitForStartFromDevice Signal: Any other message from the RFXCom device but InterfaceControlResponse
+--
+-- Just ignore it and wait for the correct message or timeout, whichever occurs first.
+--
+executeStateMachine WaitForStartFromTheDevice  msg@(Message _) = do
   env <- ask
-  state <- get  
+  state <- get
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: WaitForStartFomTheDevice ignoring wrong Message (" ++ show (timer state) ++ ")"
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: " ++ show msg
   return ()
 
+--
+-- State: WaitForStartFromDevice Signal: Timeout
+--
+-- We got a timeout, lets restart the state machine.
+--
 executeStateMachine WaitForStartFromTheDevice Timeout = do
   env <- ask
-  state <- get  
+  state <- get
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: WaitForStartFromTheDevice got Timeout (" ++ show (timer state) ++ ")"
-  liftIO $ RFXComW.send (writerH env) $ RFXComW.Message $ InterfaceControl IC.Body {IC._cmnd = IC.Reset}
-  liftIO $ putStrLn "RFXCom Device start timeout, resetting"
-  scheduleTimeout 1000000
-  put state {machineState = ResetTheDevice}
+  put state {machineState = Start}
   return ()
 
 --
 -- State: WaitForMessage Signal: Any message
 --
--- Just print the message for now
+-- Just print the message for now. But this is the main handler
 --
 executeStateMachine WaitForMessage msg = do
   env <- ask
-  state <- get  
+  state <- get
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: WaitForMessages got Message"
   Log.info $ "RFXCom.Control.RFXComMaster.executeStateMachine: " ++ show msg
   return ()
@@ -317,11 +304,11 @@ executeStateMachine WaitForMessage msg = do
 --
 -- State: Any Signal: Any
 --
--- This is the catch all state handler, i.e. if we missged something above.
+-- This is the catch all state handler, i.e. if we missed something above.
 --
 executeStateMachine st signal = do
   env <- ask
-  state <- get  
+  state <- get
   Log.info $ "Unhandled state or signal, State=" ++ show state ++ ", Signal=" ++ show signal
   return ()
 
@@ -348,12 +335,12 @@ processMasterHandler = do
 
 
 -- |Spawns off the master handler
-masterThread::Environment -- ^The environemnt that we must execute under
+masterThread::Environment -- ^The environment that we must execute under
             ->IO ()
 masterThread env = do
-  
+
   Log._info (loggerH env) "RFXCom.Control.RFXComMaster.masterThread: Master thread is up and running"
 
   -- Run the RFXCom Master
   runRFXComMaster processMasterHandler env (State Start Nothing)
-  
+
